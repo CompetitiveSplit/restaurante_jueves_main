ecfed7863315173ffb509d5d5c58e56eff214126
CompetitiveSplit <narvaez0526@gmail.com>
2024-11-06 21:11:27 -0500

    Cambios: Nuevo texto en Inicio.tsx, Acerca.tsx

--- src/app/componentes/contenedor/Inicio.tsx
+++ src/app/componentes/contenedor/Inicio.tsx
@@ -4,15 +4,13 @@ export const Inicio = () => {
       <div className="pt-5 d-flex justify-content-center">
         <div className="col-md-6">
           <div className="h-100 p-5 text-bg-dark rounded-3">
-            <h2>Change the background</h2>
+            <h2>Bienvenidos a xxxx xxxx</h2>
             <p>
-              Swap the background-color utility and add a `.text-*` color
-              utility to mix up the jumbotron look. Then, mix and match with
-              additional component themes and more.
+              Te invitamos a embarcarte en un viaje culinario por Colombia, donde cada plato
+              es una celebración de nuestras tradiciones y sabores. Disfruta de una experiencia
+              gastronómica única, diseñada para llevarte por los aromas y texturas que definen
+              cada región de nuestro país.
             </p>
-            <button className="btn btn-outline-light" type="button">
-              Example button
-            </button>
           </div>
         </div>
       </div>
--- src/app/componentes/otros/Acerca.tsx
+++ src/app/componentes/otros/Acerca.tsx
@@ -1,8 +1,16 @@
 export const Acerca = () => {
   return (
     <>
-      Soy el acerca <br />
-      El de la descripcion...
+      <div className="about-container">
+      <h1>Acerca de xxx xxx</h1>
+      <p>
+      En xxxx xxxx, nos apasiona llevar a la mesa los sabores auténticos de cada región de Colombia. Desde los platos emblemáticos de la región Andina hasta las delicias del Caribe, cada creación en nuestro menú busca capturar la esencia de nuestra tierra. Creemos que la comida es un lenguaje universal y, a través de nuestros platos, contamos historias de tradición, cultura y pasión por la cocina.
+
+      Nuestro compromiso va más allá de ofrecer platos deliciosos; queremos que cada visita sea una experiencia inolvidable, donde nuestros comensales puedan explorar la riqueza de la gastronomía colombiana en cada bocado. Nos enorgullece utilizar ingredientes frescos y de calidad, honrando las recetas tradicionales y añadiendo un toque único para sorprender los paladares más exigentes.
+
+      En xxxx xxxx, no solo servimos comida, sino momentos para recordar. ¡Te invitamos a descubrir y disfrutar con nosotros una verdadera aventura culinaria por Colombia!
+      </p>
+    </div>
     </>
   );
 };
